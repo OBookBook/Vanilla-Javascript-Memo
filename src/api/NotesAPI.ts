77c517450742f61057b4a78d70e38574d023bf65
N.I <iworksWeb@gmail.com>
2025-01-13 22:23:54 +0900

    add NotesAPI: Introduce Note interface and enhance saveNote method to manage multiple notes in localStorage

--- src/api/NotesAPI.ts
+++ src/api/NotesAPI.ts
@@ -1,10 +1,24 @@
+interface Note {
+  id?: number;
+  title: string;
+  body: string;
+  updated?: string;
+}
+
 export default class NotesAPI {
-  static saveNote(noteToSave: { title: string; body: string }): void {
-    const note = {
-      ...noteToSave,
-      id: Math.floor(Math.random() * 1000000),
-      updated: new Date().toISOString(),
-    };
-    localStorage.setItem("notes", JSON.stringify(note));
+  static getAllNotes(): Note[] {
+    const notes = JSON.parse(localStorage.getItem("notes") ?? "[]");
+
+    return notes;
+  }
+
+  static saveNote(noteToSave: Note): void {
+    const notes = this.getAllNotes();
+
+    noteToSave.id = Math.floor(Math.random() * 1000000);
+    noteToSave.updated = new Date().toISOString();
+    notes.push(noteToSave);
+
+    localStorage.setItem("notes", JSON.stringify(notes));
   }
 }
